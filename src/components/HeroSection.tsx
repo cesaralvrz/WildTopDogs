800720b6842303ff8fbdb80d12bb4d769d253d96
César Álvarez <cesaralv97@gmail.com>
2025-02-18 11:30:09 +0100

    Move button after video

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,7 +4,7 @@ export function HeroSectionDemo() {
     return (
         <div>
             <HeroSection />
-            <div className="relative mt-auto sm:mt-16">
+            <div className="relative mt-auto">
                 <div className="wistia_responsive_padding" style={{ padding: '56.25% 0 0 0', position: 'relative' }}>
                     <div className="wistia_responsive_wrapper" style={{ height: '100%', left: 0, position: 'absolute', top: 0, width: '100%' }}>
                         <iframe 
@@ -22,6 +22,11 @@ export function HeroSectionDemo() {
                     </div>
                 </div>
                 <script src="https://fast.wistia.net/assets/external/E-v1.js" async></script>
+            </div>
+            <div className='flex justify-center mt-12 mb-12'>
+                <div className="relative cursor-pointer inline-flex items-center gap-2 px-8 py-5 bg-zinc-900 dark:bg-zinc-100 text-white dark:text-zinc-900 hover:bg-zinc-800 dark:hover:bg-zinc-200 rounded-xl overflow-hidden shadow-lg shadow-zinc-900/20 dark:shadow-zinc-100/20">
+                    <a href="https://wildtopdogs.com/producto/arnes-bornwild-y-correa-2m/" className="relative z-10 flex items-center gap-2 text-xl">Consigue tu pack</a>
+                </div>
             </div>
         </div>
     )
--- src/components/ui/hero-section.tsx
+++ src/components/ui/hero-section.tsx
@@ -1,11 +1,6 @@
-import React from 'react'
-
-
-
-
 const HeroSection = () => {
     return (
-        <div className='flex w-full h-[50vh] gap-4 p-4'>
+        <div className='flex w-full h-[35vh] sm:h-[50vh] gap-4 p-4'>
             <div className='flex flex-col min-h-screen items-center mt-[2.5rem] p-3 w-full'>
                 <h1 className="scroll-m-20 max-w-[500px] text-3xl md:text-5xl font-bold tracking-tight leading-[45px] md:leading-[70px] text-center">
                     Descubre 3 secretos infalibles para pasear a tu perro <span className="p-2 bg-purple-100 rounded-[8px]">sin estrés</span>
@@ -13,14 +8,7 @@ const HeroSection = () => {
                 <p className="mx-auto max-w-[600px] text-gray-500 md:text-lg text-center mt-2 dark:text-gray-400">
                     Te ofrecemos la mejor experiencia paseando a tu perro.
                 </p>
-
-                <div className='flex gap-2 mt-2 mb-12'>
-                    <div className="relative cursor-pointer inline-flex items-center gap-2 px-8 py-5 bg-zinc-900 dark:bg-zinc-100 text-white dark:text-zinc-900 hover:bg-zinc-800 dark:hover:bg-zinc-200 rounded-xl overflow-hidden shadow-lg shadow-zinc-900/20 dark:shadow-zinc-100/20 mt-4">
-                        <a href="https://wildtopdogs.com/producto/arnes-bornwild-y-correa-2m/" className="relative z-10 flex items-center gap-2 text-xl">Consigue tu pack</a>
-                    </div>
-                </div>
             </div>
-
         </div>
     )
 }
